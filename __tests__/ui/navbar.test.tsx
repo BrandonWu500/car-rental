121f7ad06af3213320adaa34c7105768ef501431
BrandonWu500 <brwu17@gmail.com>
2023-06-15 11:45:24 -0400

    test and code search bar

--- __tests__/ui/navbar.test.tsx
+++ __tests__/ui/navbar.test.tsx
@@ -9,4 +9,18 @@ describe("Navbar", () => {
     expect(logo).toBeInTheDocument();
     expect(logo).toHaveAttribute("href", "/");
   });
+
+  it("shows a search bar with filters and search button", () => {
+    render(<Navbar />);
+
+    const locationText = screen.getByText(/anywhere/i);
+    const dateText = screen.getByText(/any week/i);
+    const passengerText = screen.getByText(/add passengers/i);
+    const searchBtn = screen.getByRole("button", { name: /search/i });
+
+    expect(locationText).toBeInTheDocument();
+    expect(dateText).toBeInTheDocument();
+    expect(passengerText).toBeInTheDocument();
+    expect(searchBtn).toBeInTheDocument();
+  });
 });
