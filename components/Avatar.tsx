82c46c8fddb3a2a8cb39db17ad628aed90181312
BrandonWu500 <brwu17@gmail.com>
2023-06-15 12:46:05 -0400

    test and code user menu

---
+++ components/Avatar.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+interface AvatarProps {
+  src?: string | null | undefined;
+}
+
+const Avatar = ({ src }: AvatarProps) => {
+  return (
+    <Image
+      src={src ?? "/blank-profile-picture.png"}
+      alt="user avatar"
+      className="rounded-full"
+      width={30}
+      height={30}
+    />
+  );
+};
+export default Avatar;
