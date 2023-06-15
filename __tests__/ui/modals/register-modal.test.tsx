93b5cc98e7300e3319eac24decd7296618418f55
BrandonWu500 <brwu17@gmail.com>
2023-06-15 17:22:09 -0400

    zustand setup and add heading

--- __tests__/ui/modals/register-modal.test.tsx
+++ __tests__/ui/modals/register-modal.test.tsx
@@ -1,8 +1,23 @@
 import RegisterModal from "@/components/modals/RegisterModal";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 describe("RegisterModal", () => {
   it("displays a heading", () => {
     render(<RegisterModal />);
+
+    const title = screen.getByRole("heading", {
+      name: /Welcome to Car Rental!/i,
+    });
+    const subtitle = screen.getByText(/Create an account/i);
+
+    expect(title).toBeInTheDocument();
+    expect(subtitle).toBeInTheDocument();
+  });
+
+  it("should have an email input", () => {
+    render(<RegisterModal />);
+
+    const emailInput = screen.getByRole("textbox", { name: /email/i });
+    expect(emailInput).toBeInTheDocument();
   });
 });
--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -1,4 +1,13 @@
+import { useRegisterModal } from "@/hooks/useRegisterModal";
+import Heading from "../Heading";
+
 const RegisterModal = () => {
-  return <div>RegisterModal</div>;
+  const registerModal = useRegisterModal();
+
+  return (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to Car Rental!" subtitle="Create an account" />
+    </div>
+  );
 };
 export default RegisterModal;
