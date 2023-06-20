5bad775e4b0e58f7a29016eba7f42379341b8c38
BrandonWu500 <brwu17@gmail.com>
2023-06-20 18:38:47 -0400

    refactor: google btn in modals

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -1,17 +1,17 @@
-import { useCallback, useState } from "react";
-import { FcGoogle } from "react-icons/fc";
-import { signIn } from "next-auth/react";
-import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
-import axios from "axios";
+import { useCallback, useState } from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import { signIn } from 'next-auth/react';
+import { useForm, FieldValues, SubmitHandler } from 'react-hook-form';
+import axios from 'axios';
 
-import { useRegisterModal } from "@/hooks/useRegisterModal";
+import { useRegisterModal } from '@/hooks/useRegisterModal';
 
-import Heading from "../Heading";
-import Input from "../inputs/Input";
-import Modal from "./Modal";
-import Button from "../Button";
-import { toast } from "react-hot-toast";
-import { useLoginModal } from "@/hooks/useLoginModal";
+import Heading from '../Heading';
+import Input from '../inputs/Input';
+import Modal from './Modal';
+import Button from '../Button';
+import { toast } from 'react-hot-toast';
+import { useLoginModal } from '@/hooks/useLoginModal';
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -25,9 +25,9 @@ const RegisterModal = () => {
     formState: { errors },
   } = useForm<FieldValues>({
     defaultValues: {
-      name: "",
-      email: "",
-      password: "",
+      name: '',
+      email: '',
+      password: '',
     },
   });
 
@@ -35,8 +35,8 @@ const RegisterModal = () => {
     setIsLoading(true);
 
     try {
-      await axios.post("/api/register", data);
-      toast.success("Registered!");
+      await axios.post('/api/register', data);
+      toast.success('Registered!');
       registerModal.onClose();
       loginModal.onOpen();
     } catch (error: any) {
@@ -84,10 +84,10 @@ const RegisterModal = () => {
     <div className="flex flex-col gap-4 mt-3">
       <hr />
       <Button
-        outline
+        intent="secondary"
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => signIn("google")}
+        onClick={() => signIn('google')}
       />
       <div
         className="text-neutral-500 text-center
