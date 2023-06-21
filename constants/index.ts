4bec53200648b221e938e572244c9d0a7bd08b53
BrandonWu500 <brwu17@gmail.com>
2023-06-21 11:37:31 -0400

    feat: add static categories

---
+++ constants/index.ts
@@ -0,0 +1,26 @@
+export const CATEGORIES = [
+  {
+    label: 'Sedans',
+  },
+  {
+    label: 'Crossovers & SUVs',
+  },
+  {
+    label: 'Coupes',
+  },
+  {
+    label: 'Convertibles',
+  },
+  {
+    label: 'Supercars',
+  },
+  {
+    label: 'Electric',
+  },
+  {
+    label: 'Hybrid',
+  },
+  {
+    label: 'Luxury',
+  },
+];
