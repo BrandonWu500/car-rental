a1fc23d7a0d71077284a2a9c374e8fb703ebd289
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-06-22 08:45:10 -0400

    feat: add category selection to create listing modal (#37)

--- components/modals/CreateListingModal.tsx
+++ components/modals/CreateListingModal.tsx
@@ -1,8 +1,14 @@
+import axios from 'axios';
+import { useRouter } from 'next/router';
 import { useMemo, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
 
 import { useCreateListingModal } from '@/hooks/useCreateListingModal';
 
+import { CATEGORIES } from '@/constants';
+import Heading from '../Heading';
+import CategoryInput from '../inputs/CategoryInput';
 import Modal from './Modal';
 
 enum STEPS {
@@ -15,6 +21,7 @@ enum STEPS {
 }
 
 const CreateListingModal = () => {
+  const router = useRouter();
   const createListingModal = useCreateListingModal();
 
   const [isLoading, setIsLoading] = useState(false);
@@ -23,10 +30,25 @@ const CreateListingModal = () => {
   const {
     register,
     handleSubmit,
+    watch,
+    setValue,
+    reset,
     formState: { errors },
   } = useForm<FieldValues>({
-    defaultValues: {},
+    defaultValues: {
+      category: '',
+    },
   });
+
+  const category = watch('category');
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldDirty: true,
+      shouldTouch: true,
+      shouldValidate: true,
+    });
+  };
 
   const onBack = () => {
     setStep((value) => value - 1);
@@ -37,12 +59,23 @@ const CreateListingModal = () => {
   };
 
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
-    // setIsLoading(true);
-    // try {
-    // } catch (error: any) {
-    // } finally {
-    //   setIsLoading(false);
-    // }
+    if (step !== STEPS.PRICE) {
+      return onNext();
+    }
+
+    setIsLoading(true);
+    try {
+      await axios.post('/api/listings', data);
+      toast.success('Listing created!');
+      router.reload();
+      reset();
+      setStep(STEPS.CATEGORY);
+      createListingModal.onClose();
+    } catch (error: any) {
+      toast.error('Something went wrong.');
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const actionLabel = useMemo(() => {
@@ -53,7 +86,36 @@ const CreateListingModal = () => {
     return 'Next';
   }, [step]);
 
-  const bodyContent = <div className="flex flex-col gap-4"></div>;
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
+      return undefined;
+    }
+
+    return 'Back';
+  }, [step]);
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading
+        title="Which of these best describes your car?"
+        subtitle="Pick a category"
+      />
+      <div
+        className="grid max-h-[50vh] grid-cols-1 gap-3
+      overflow-y-auto md:grid-cols-2"
+      >
+        {CATEGORIES.map((item) => (
+          <div key={item.label} className="col-span-1">
+            <CategoryInput
+              onClick={(category) => setCustomValue('category', category)}
+              selected={category === item.label}
+              label={item.label}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 
   const footerContent = <div className="mt-3 flex flex-col gap-4"></div>;
 
@@ -65,6 +127,7 @@ const CreateListingModal = () => {
       actionLabel={actionLabel}
       onClose={createListingModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
     />
   );
 };
