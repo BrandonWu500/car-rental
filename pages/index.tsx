037a721759c3d2a7373c1c5c18fd57cc1be1876a
BrandonWu500 <brwu17@gmail.com>
2023-06-23 12:29:56 -0400

    feat: add type safe listings to home
    through isr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,40 @@
-import RegisterModal from '@/components/modals/RegisterModal';
-import Navbar from '@/components/navbar/Navbar';
+import Container from '@/components/Container';
 
-const Home = () => {
-  return <></>;
+import { prisma } from '@/libs/prismadb';
+import { TypeSafeListing } from '@/types';
+import { GetStaticProps } from 'next';
+
+export const getStaticProps: GetStaticProps = async () => {
+  const listings = await prisma.listing.findMany();
+
+  const typeSafeListings = listings.map((listing) => ({
+    ...listing,
+    createdAt: listing.createdAt.toISOString(),
+  }));
+
+  return {
+    props: { listings: typeSafeListings },
+    revalidate: 5,
+  };
+};
+
+interface HomeProps {
+  listings: TypeSafeListing[];
+}
+
+const Home = ({ listings }: HomeProps) => {
+  return (
+    <Container>
+      <div
+        className="grid grid-cols-1 gap-8 pt-24
+    sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5
+    2xl:grid-cols-6"
+      >
+        {listings?.map((listing) => (
+          <div key={listing.id}>{listing.make}</div>
+        ))}
+      </div>
+    </Container>
+  );
 };
 export default Home;
--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,12 @@
+import { Listing } from '@prisma/client';
+
 export type CountrySelectValue = {
   flag: string;
   label: string;
   region: string;
   value: string;
+};
+
+export type TypeSafeListing = Omit<Listing, 'createdAt'> & {
+  createdAt: string;
 };
