f84f9bc253704acb76aef894267a2f8bb8091d2d
BrandonWu500 <brwu17@gmail.com>
2023-06-23 13:45:30 -0400

    feat: create favorite listing api endpoints

---
+++ pages/api/favorites/[listingId].ts
@@ -0,0 +1,68 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { serverAuth } from '@/libs/serverAuth';
+import { prisma } from './../../../libs/prismadb';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const currentUser = await serverAuth(req, res);
+
+    if (!currentUser) {
+      throw new Error('Need to be logged in.');
+    }
+
+    if (req.method === 'POST') {
+      const { listingId } = req.query;
+
+      if (!listingId || typeof listingId !== 'string') {
+        throw new Error('Invalid ID');
+      }
+
+      let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+      favoriteIds.push(listingId);
+
+      await prisma.user.update({
+        where: {
+          id: currentUser.id,
+        },
+        data: {
+          favoriteIds,
+        },
+      });
+
+      return res.json({ message: 'Liked!' });
+    }
+
+    if (req.method === 'DELETE') {
+      const { listingId } = req.query;
+
+      if (!listingId || typeof listingId !== 'string') {
+        throw new Error('Invalid ID');
+      }
+
+      let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+      favoriteIds = favoriteIds.filter((id) => id !== listingId);
+
+      await prisma.user.update({
+        where: {
+          id: currentUser.id,
+        },
+        data: {
+          favoriteIds,
+        },
+      });
+
+      return res.json({ message: 'Unliked!' });
+    }
+
+    return res.status(405).end();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).end();
+  }
+}
