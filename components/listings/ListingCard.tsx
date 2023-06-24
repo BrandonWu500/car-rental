401e6cc68e9b82370ceb8580d5d15dddbad8a75f
BrandonWu500 <brwu17@gmail.com>
2023-06-24 12:14:56 -0400

    feat: add listing card info

--- components/listings/ListingCard.tsx
+++ components/listings/ListingCard.tsx
@@ -1,3 +1,4 @@
+import { useCountries } from '@/hooks/useCountries';
 import { TypeSafeListing } from '@/types';
 import Image from 'next/image';
 
@@ -6,10 +7,14 @@ interface ListingCardProps {
 }
 
 const ListingCard = ({ listing }: ListingCardProps) => {
+  const { getByValue } = useCountries();
+
+  const location = getByValue(listing.locationValue);
+
   return (
     <div className="group col-span-1 cursor-pointer">
       <div className="flex w-full flex-col gap-2">
-        <div className="relative aspect-square w-full overflow-hidden rounded-xl">
+        <div className="relative mb-2 aspect-square w-full max-w-[400px] overflow-hidden rounded-xl">
           <Image
             fill
             className="object-cover transition group-hover:scale-110"
@@ -17,6 +22,15 @@ const ListingCard = ({ listing }: ListingCardProps) => {
             alt="Listing"
           />
         </div>
+        <h3 className="text-xl font-bold">
+          {listing.make} {listing.model}
+        </h3>
+        <p className="text-lg font-semibold">{listing.trim}</p>
+        <p className="text-lg font-semibold">
+          {location?.region}, {location?.label}
+        </p>
+
+        <p className="font-semibold">$ {listing.price} / hr</p>
       </div>
     </div>
   );
