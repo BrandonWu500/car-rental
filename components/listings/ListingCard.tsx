74391aecc17d84c28994cdba19eb0e442ea819f2
BrandonWu500 <brwu17@gmail.com>
2023-06-26 16:09:31 -0400

    feat: update listing card
    add reservation specific info

--- components/listings/ListingCard.tsx
+++ components/listings/ListingCard.tsx
@@ -2,19 +2,39 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 
 import { useCountries } from '@/hooks/useCountries';
-import { SafeTypeListing } from '@/types';
+import { SafeTypeListing, SafeTypeReservation } from '@/types';
 
+import { differenceInDays, format } from 'date-fns';
+import { useMemo } from 'react';
 import FavoriteButton from '../FavoriteButton';
 
 interface ListingCardProps {
   listing: SafeTypeListing;
+  reservation?: SafeTypeReservation;
 }
 
-const ListingCard = ({ listing }: ListingCardProps) => {
+const ListingCard = ({ listing, reservation }: ListingCardProps) => {
   const router = useRouter();
   const { getByValue } = useCountries();
 
   const location = getByValue(listing.locationValue);
+
+  const price = useMemo(() => {
+    return reservation ? reservation.totalPrice : listing.price;
+  }, [reservation, listing.price]);
+
+  const reservationDate = useMemo(() => {
+    if (!reservation) return null;
+
+    const start = new Date(reservation.startDate);
+    const end = new Date(reservation.endDate);
+
+    const diffInDays = differenceInDays(end, start);
+
+    return diffInDays > 1
+      ? `${format(start, 'PP')} - ${format(end, 'PP')} `
+      : `${format(start, 'PP')}`;
+  }, [reservation]);
 
   return (
     <div
@@ -41,7 +61,14 @@ const ListingCard = ({ listing }: ListingCardProps) => {
           {location?.region}, {location?.label}
         </p>
 
-        <p className="font-semibold">$ {listing.price} / hr</p>
+        <p className="font-light text-neutral-500">
+          {reservationDate ?? listing.category}
+        </p>
+
+        <div className="flex gap-1">
+          <p className="font-semibold">$ {price} </p>
+          {!reservation && <p className="font-light">/ day</p>}
+        </div>
       </div>
     </div>
   );
