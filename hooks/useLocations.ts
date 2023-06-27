0841293ece51f0b057e533a889c50c6ca241b3a9
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-06-27 13:19:16 -0400

    feat: update location select (#60)
    also add more storybook tests

--- hooks/useLocations.ts
+++ hooks/useLocations.ts
@@ -1,23 +1,20 @@
-import { LOCATION_TYPE } from '@/types';
 import { City, State } from 'country-state-city';
 
 const COUNTRY_CODE = 'US';
 
-export const useLocations = (
-  type: LOCATION_TYPE,
-  stateCode: string
-): string[] => {
-  if (type === LOCATION_TYPE.CITY) {
-    const cities = City.getCitiesOfState(COUNTRY_CODE, stateCode);
-    const cityNames = cities.map((city) => city.name);
-    return cityNames;
-  }
-
-  if (type === LOCATION_TYPE.STATE) {
+export const useLocations = () => {
+  const getStates = () => {
     const states = State.getStatesOfCountry(COUNTRY_CODE);
-    const stateNames = states.map((state) => state.name);
-    return stateNames;
-  }
+    const stateCodes = states?.map((state) => state.isoCode) ?? [];
+    return stateCodes;
+  };
 
-  return [];
+  const getCitiesByState = (stateCode: string) => {
+    const cities = City.getCitiesOfState(COUNTRY_CODE, stateCode);
+    const cityNames =
+      cities?.map((city) => `${city.name}, ${city.stateCode}`) ?? [];
+    return cityNames;
+  };
+
+  return { getStates, getCitiesByState };
 };
--- stories/LocationSelect.stories.tsx
+++ stories/LocationSelect.stories.tsx
@@ -1,35 +1,94 @@
-import LocationSelect from '@/components/inputs/LocationSelect';
-import { LOCATION_TYPE } from '@/types';
+import { expect } from '@storybook/jest';
 import type { Meta, StoryObj } from '@storybook/react';
+import { userEvent, within } from '@storybook/testing-library';
+
+import { LOCATION_TYPE } from '@/types';
+
+import LocationSelect from '@/components/inputs/LocationSelect';
 
 //👇 This default export determines where your story goes in the story list
 const meta: Meta<typeof LocationSelect> = {
   component: LocationSelect,
   tags: ['autodocs'],
-  argTypes: {
-    type: {
-      options: [LOCATION_TYPE.STATE, LOCATION_TYPE.CITY], // iterator
-      mapping: [LOCATION_TYPE.STATE, LOCATION_TYPE.CITY], // values
-      control: {
-        type: 'radio',
-        labels: ['state', 'city'],
-      },
-    },
-  },
 };
 
 export default meta;
 type Story = StoryObj<typeof LocationSelect>;
 
-export const State: Story = {
+export const EmptyState: Story = {
   args: {
+    value: '',
+    onChange: () => {},
     type: LOCATION_TYPE.STATE,
   },
 };
 
-export const City: Story = {
+export const SelectedState: Story = {
+  args: {
+    value: 'MA',
+    onChange: () => {},
+    type: LOCATION_TYPE.STATE,
+  },
+};
+
+export const SelectStateTest: Story = {
+  args: {
+    type: LOCATION_TYPE.STATE,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const input = canvas.getByPlaceholderText('State');
+    await userEvent.click(input);
+    await userEvent.type(input, 'ma');
+    const option = await canvas.findByText('MA');
+    await userEvent.click(option);
+    await expect(option).toBeInTheDocument();
+  },
+};
+
+export const EmptyCity: Story = {
+  args: {
+    value: '',
+    onChange: () => {},
+    type: LOCATION_TYPE.CITY,
+  },
+};
+
+export const SelectedCity: Story = {
+  args: {
+    value: 'Lexington, MA',
+    onChange: () => {},
+    type: LOCATION_TYPE.CITY,
+  },
+};
+
+export const SelectCityTest: Story = {
   args: {
     type: LOCATION_TYPE.CITY,
-    stateCode: 'MA',
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const input = canvas.getByPlaceholderText('City');
+    await userEvent.click(input);
+    await userEvent.type(input, 'Concord');
+    const option = await canvas.findByText(/West Concord, MA/i);
+    await userEvent.click(option);
+    await expect(option).toBeInTheDocument();
+  },
+};
+
+export const DiffStateCodeTest: Story = {
+  args: {
+    stateCode: 'CA',
+    type: LOCATION_TYPE.CITY,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const input = canvas.getByPlaceholderText('City');
+    await userEvent.click(input);
+    await userEvent.type(input, 'San Francisco');
+    const option = await canvas.findByText('San Francisco, CA');
+    await userEvent.click(option);
+    await expect(option).toBeInTheDocument();
   },
 };
