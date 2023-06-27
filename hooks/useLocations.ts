45966b932d421dc0340534f617c0971b50f1256f
BrandonWu500 <brwu17@gmail.com>
2023-06-27 14:51:33 -0400

    feat: update useLocations hook
    filter out minor islands from getStates

--- hooks/useLocations.ts
+++ hooks/useLocations.ts
@@ -5,14 +5,16 @@ const COUNTRY_CODE = 'US';
 export const useLocations = () => {
   const getStates = () => {
     const states = State.getStatesOfCountry(COUNTRY_CODE);
-    const stateCodes = states?.map((state) => state.isoCode) ?? [];
+    const filteredStates = states?.filter(
+      (state) => !state.isoCode.includes('UM')
+    );
+    const stateCodes = filteredStates?.map((state) => state.isoCode) ?? [];
     return stateCodes;
   };
 
   const getCitiesByState = (stateCode: string) => {
     const cities = City.getCitiesOfState(COUNTRY_CODE, stateCode);
-    const cityNames =
-      cities?.map((city) => `${city.name}, ${city.stateCode}`) ?? [];
+    const cityNames = cities?.map((city) => `${city.name}`) ?? [];
     return cityNames;
   };
 
