dd77f40dde5b1f167da389550ab35247c8d642ea
BrandonWu500 <brwu17@gmail.com>
2023-07-04 07:40:30 -0400

    chore: add createTestUser to resetDB

--- prisma/reset-db.ts
+++ prisma/reset-db.ts
@@ -1,9 +1,21 @@
+import { hash } from 'bcrypt';
 import { prisma } from './../libs/prismadb';
 
 const deleteAll = async () => {
   await prisma.user.deleteMany();
 };
 
+const createTestUser = async (name: string, password: string) => {
+  await prisma.user.create({
+    data: {
+      email: `${name}@test.com`,
+      name,
+      hashedPassword: await hash(password, 12),
+    },
+  });
+};
+
 export const resetDB = async () => {
   await deleteAll();
+  await createTestUser('john', '123456');
 };
