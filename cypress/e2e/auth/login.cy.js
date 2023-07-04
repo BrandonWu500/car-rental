e8892f1183f66fba114f08832f6008889442c636
BrandonWu500 <brwu17@gmail.com>
2023-07-04 08:00:14 -0400

    test: invalid credentials flow

--- cypress/e2e/auth/login.cy.js
+++ cypress/e2e/auth/login.cy.js
@@ -26,27 +26,37 @@ describe('Login', () => {
     cy.findByText(/logout/i).should('exist');
   });
 
-  // it('should should an error message with missing or invalid credentials', () => {
-  //   cy.task('db:reset');
+  it('should show an error message with invalid credentials', () => {
+    cy.task('db:reset');
 
-  //   cy.visit('/');
+    cy.visit('/');
 
-  //   cy.findByRole('button', { name: /user menu/i }).click();
+    cy.findByRole('button', { name: /user menu/i }).click();
 
-  //   cy.findByText(/login/i).click();
+    cy.findByText(/login/i).click();
 
-  //   cy.findByRole('heading', { name: 'Login' }).should('exist');
+    cy.findByRole('heading', { name: 'Login' }).should('exist');
 
-  //   cy.findByLabelText(/email/i).clear();
-  //   cy.findByLabelText(/email/i).type('john@test.com');
+    // INVALID EMAIL
+    cy.findByLabelText(/email/i).clear();
+    cy.findByLabelText(/email/i).type('john1@test.com');
 
-  //   cy.findByLabelText(/password/i).clear();
-  //   cy.findByLabelText(/password/i).type('123456');
+    cy.findByLabelText(/password/i).clear();
+    cy.findByLabelText(/password/i).type('123456');
 
-  //   cy.findByRole('button', { name: 'Continue' }).click();
+    cy.findByRole('button', { name: 'Continue' }).click();
 
-  //   // LOGOUT
-  //   cy.findByRole('button', { name: /user menu/i }).click();
-  //   cy.findByText(/logout/i).should('exist');
-  // });
+    cy.findByText(/Invalid Credentials/i);
+
+    // INVALID PASSWORD
+    cy.findByLabelText(/email/i).clear();
+    cy.findByLabelText(/email/i).type('john@test.com');
+
+    cy.findByLabelText(/password/i).clear();
+    cy.findByLabelText(/password/i).type('12345');
+
+    cy.findByRole('button', { name: 'Continue' }).click();
+
+    cy.findByText(/Invalid Credentials/i);
+  });
 });
