a65a32220350c996d6740ed35f5cdd1d85760692
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-07-04 09:50:07 -0400

    test: signup error flows (#90)

--- cypress/e2e/auth/signup.cy.js
+++ cypress/e2e/auth/signup.cy.js
@@ -1,40 +1,94 @@
 /// <reference types="Cypress" />
 
-it('runs auth flow for user sign up, login, and logout', () => {
-  cy.task('db:reset');
+describe('Signup', () => {
+  it('runs auth flow for user sign up, login, and logout', () => {
+    cy.task('db:reset');
 
-  cy.visit('/');
+    cy.visit('/');
 
-  // SIGN UP
-  cy.findByRole('button', { name: /user menu/i }).click();
+    // SIGN UP
+    cy.findByRole('button', { name: /user menu/i }).click();
 
-  cy.findByText(/sign up/i).click();
+    cy.findByText(/sign up/i).click();
 
-  cy.findByRole('heading', { name: 'Register' }).should('exist');
+    cy.findByRole('heading', { name: 'Register' }).should('exist');
 
-  cy.findByLabelText(/email/i).clear();
-  cy.findByLabelText(/email/i).type('test@test.com');
+    cy.findByLabelText(/email/i).clear();
+    cy.findByLabelText(/email/i).type('test@test.com');
 
-  cy.findByLabelText(/name/i).clear();
-  cy.findByLabelText(/name/i).type('test');
+    cy.findByLabelText(/name/i).clear();
+    cy.findByLabelText(/name/i).type('test');
 
-  cy.findByLabelText(/password/i).clear();
-  cy.findByLabelText(/password/i).type('test');
+    cy.findByLabelText(/password/i).clear();
+    cy.findByLabelText(/password/i).type('test');
 
-  cy.findByRole('button', { name: 'Continue' }).click();
+    cy.findByRole('button', { name: 'Continue' }).click();
 
-  // LOGIN WITH NEWLY CREATED USER
-  cy.findByRole('heading', { name: 'Login' }).should('exist');
+    // LOGIN WITH NEWLY CREATED USER
+    cy.findByRole('heading', { name: 'Login' }).should('exist');
 
-  cy.findByLabelText(/email/i).clear();
-  cy.findByLabelText(/email/i).type('test@test.com');
+    cy.findByLabelText(/email/i).clear();
+    cy.findByLabelText(/email/i).type('test@test.com');
 
-  cy.findByLabelText(/password/i).clear();
-  cy.findByLabelText(/password/i).type('test');
+    cy.findByLabelText(/password/i).clear();
+    cy.findByLabelText(/password/i).type('test');
 
-  cy.findByRole('button', { name: 'Continue' }).click();
+    cy.findByRole('button', { name: 'Continue' }).click();
 
-  // LOGOUT
-  cy.findByRole('button', { name: /user menu/i }).click();
-  cy.findByText(/logout/i).click();
+    // LOGOUT
+    cy.findByRole('button', { name: /user menu/i }).click();
+    cy.findByText(/logout/i).click();
+  });
+
+  it('should show an error message if email already taken', () => {
+    cy.task('db:reset');
+
+    cy.visit('/');
+
+    // SIGN UP WITH USER THAT DB WAS SEEDED WITH
+    cy.findByRole('button', { name: /user menu/i }).click();
+
+    cy.findByText(/sign up/i).click();
+
+    cy.findByRole('heading', { name: 'Register' }).should('exist');
+
+    cy.findByLabelText(/email/i).clear();
+    cy.findByLabelText(/email/i).type('john@test.com');
+
+    cy.findByLabelText(/name/i).clear();
+    cy.findByLabelText(/name/i).type('test');
+
+    cy.findByLabelText(/password/i).clear();
+    cy.findByLabelText(/password/i).type('test');
+
+    cy.findByRole('button', { name: 'Continue' }).click();
+
+    cy.findByText(/Email taken/i).should('exist');
+  });
+
+  it('should show an error message if email is invalid', () => {
+    cy.task('db:reset');
+
+    cy.visit('/');
+
+    // SIGN UP WITH INVALID EMAIL
+    cy.findByRole('button', { name: /user menu/i }).click();
+
+    cy.findByText(/sign up/i).click();
+
+    cy.findByRole('heading', { name: 'Register' }).should('exist');
+
+    cy.findByLabelText(/email/i).clear();
+    cy.findByLabelText(/email/i).type('john@test');
+
+    cy.findByLabelText(/name/i).clear();
+    cy.findByLabelText(/name/i).type('test');
+
+    cy.findByLabelText(/password/i).clear();
+    cy.findByLabelText(/password/i).type('test');
+
+    cy.findByRole('button', { name: 'Continue' }).click();
+
+    cy.findByText(/Invalid email/i).should('exist');
+  });
 });
