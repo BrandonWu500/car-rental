914d29f95899da6df6719482750d41c84214c257
BrandonWu500 <brwu17@gmail.com>
2023-07-05 08:59:13 -0400

    test: navigation to favorites page

--- cypress/e2e/routes/navigation.cy.ts
+++ cypress/e2e/routes/navigation.cy.ts
@@ -30,6 +30,19 @@ describe('Navigation', () => {
     // since db is seeded with no listings
     cy.findByRole('heading', { name: /no exact matches/i }).should('exist');
   });
+
+  it('should navigate to the favorites page', () => {
+    cy.login();
+
+    cy.findByRole('button', { name: /user menu/i }).click();
+
+    cy.findByText(/my favorites/i).click();
+
+    cy.url().should('include', '/favorites');
+
+    // since user db is seeded with has no favorites
+    cy.findByRole('heading', { name: /no favorites found/i }).should('exist');
+  });
 });
 
 export {};
