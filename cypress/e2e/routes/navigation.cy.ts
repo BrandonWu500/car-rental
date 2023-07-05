b05049695f4683383edbe6e65b5a4eae6286f59d
BrandonWu500 <brwu17@gmail.com>
2023-07-05 08:24:41 -0400

    test: navigation to trips page

--- cypress/e2e/routes/navigation.cy.ts
+++ cypress/e2e/routes/navigation.cy.ts
@@ -2,6 +2,19 @@ describe('Navigation', () => {
   beforeEach(() => {
     cy.resetDB();
   });
+
+  it('should navigate to the trips page', () => {
+    cy.login();
+
+    cy.findByRole('button', { name: /user menu/i }).click();
+
+    cy.findByText(/my trips/i).click();
+
+    cy.url().should('include', '/trips');
+
+    // since user db is seeded with has no trips
+    cy.findByRole('heading', { name: /no trips found/i }).should('exist');
+  });
 });
 
 export {};
