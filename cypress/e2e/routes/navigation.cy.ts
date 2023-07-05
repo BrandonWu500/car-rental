d0c3ee4df7b961ffcbe7140a391d8ce56135de3c
BrandonWu500 <brwu17@gmail.com>
2023-07-05 08:25:32 -0400

    test: navigation to home page

--- cypress/e2e/routes/navigation.cy.ts
+++ cypress/e2e/routes/navigation.cy.ts
@@ -15,6 +15,21 @@ describe('Navigation', () => {
     // since user db is seeded with has no trips
     cy.findByRole('heading', { name: /no trips found/i }).should('exist');
   });
+
+  it('should navigate to home page', () => {
+    cy.login();
+
+    cy.findByRole('button', { name: /user menu/i }).click();
+
+    cy.findByText(/my trips/i).click();
+
+    cy.findByRole('link', { name: /car rental/i }).click();
+
+    cy.url().should('eq', Cypress.config().baseUrl + '/');
+
+    // since db is seeded with no listings
+    cy.findByRole('heading', { name: /no exact matches/i }).should('exist');
+  });
 });
 
 export {};
