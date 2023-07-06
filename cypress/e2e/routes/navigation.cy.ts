30ee97f3217f74fd277caccec0f855e2b89ecc43
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-07-06 11:24:32 -0400

    test: basic navigation (#95)

---
+++ cypress/e2e/routes/navigation.cy.ts
@@ -0,0 +1,61 @@
+describe('Navigation', () => {
+  beforeEach(() => {
+    cy.resetDB();
+  });
+
+  it('should navigate to the trips page', () => {
+    cy.login();
+
+    cy.findByRole('button', { name: /user menu/i }).click();
+
+    cy.findByText(/my trips/i).click();
+
+    cy.url().should('include', '/trips');
+
+    // since user db is seeded with has no trips
+    cy.findByRole('heading', { name: /no trips found/i }).should('exist');
+  });
+
+  it('should navigate to home page', () => {
+    cy.login();
+
+    cy.findByRole('button', { name: /user menu/i }).click();
+
+    cy.findByText(/my trips/i).click();
+
+    cy.findByRole('link', { name: /car rental/i }).click();
+
+    cy.url().should('eq', Cypress.config().baseUrl + '/');
+
+    // since db is seeded with no listings
+    cy.findByRole('heading', { name: /no exact matches/i }).should('exist');
+  });
+
+  it('should navigate to the favorites page', () => {
+    cy.login();
+
+    cy.findByRole('button', { name: /user menu/i }).click();
+
+    cy.findByText(/my favorites/i).click();
+
+    cy.url().should('include', '/favorites');
+
+    // since user db is seeded with has no favorites
+    cy.findByRole('heading', { name: /no favorites found/i }).should('exist');
+  });
+
+  it('should navigate to the cars page', () => {
+    cy.login();
+
+    cy.findByRole('button', { name: /user menu/i }).click();
+
+    cy.findByText(/my cars/i).click();
+
+    cy.url().should('include', '/cars');
+
+    // since user db is seeded with has no cars
+    cy.findByRole('heading', { name: /no cars found/i }).should('exist');
+  });
+});
+
+export {};
