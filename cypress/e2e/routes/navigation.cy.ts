90ed18bfe89c4e3cf10c1a719397467961226448
BrandonWu500 <brwu17@gmail.com>
2023-07-06 07:56:50 -0400

    test: navigation to cars page

--- cypress/e2e/routes/navigation.cy.ts
+++ cypress/e2e/routes/navigation.cy.ts
@@ -43,6 +43,19 @@ describe('Navigation', () => {
     // since user db is seeded with has no favorites
     cy.findByRole('heading', { name: /no favorites found/i }).should('exist');
   });
+
+  it('should navigate to the cars page', () => {
+    cy.login();
+
+    cy.findByRole('button', { name: /user menu/i }).click();
+
+    cy.findByText(/my cars/i).click();
+
+    cy.url().should('include', '/cars');
+
+    // since user db is seeded with has no cars
+    cy.findByRole('heading', { name: /no cars found/i }).should('exist');
+  });
 });
 
 export {};
