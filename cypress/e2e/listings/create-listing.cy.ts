fc8b3480d3ed121d507b2fedc9f31fae76dd5758
BrandonWu500 <brwu17@gmail.com>
2023-07-06 11:37:02 -0400

    test: open modal when logged in

--- cypress/e2e/listings/create-listing.cy.ts
+++ cypress/e2e/listings/create-listing.cy.ts
@@ -3,6 +3,19 @@
 describe('Create Listing Modal', () => {
   beforeEach(() => {
     cy.resetDB();
+    cy.intercept('GET', '/api/current-user').as('getCurrentUser');
+  });
+
+  it('should open when "rent out your car" is clicked', () => {
+    cy.login();
+
+    // first fetch returns null, so need to wait for 2nd fetch
+    cy.wait('@getCurrentUser');
+    cy.wait('@getCurrentUser');
+
+    cy.findByText(/rent out your car/i).click();
+
+    cy.findByRole('heading', { name: 'Rent out your car!' }).should('exist');
   });
 });
 
