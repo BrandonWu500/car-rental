cbc058356a0850b11fe98e4ac187244e48a6a871
BrandonWu500 <brwu17@gmail.com>
2023-07-07 17:44:20 -0400

    refactor: add conditional testid
    based on if there is current user

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -9,7 +9,10 @@ const Navbar = () => {
   const { data: currentUser } = useCurrentUser();
 
   return (
-    <div className="fixed z-10 w-full bg-white shadow-sm">
+    <div
+      className="fixed z-10 w-full bg-white shadow-sm"
+      data-testid={currentUser ? 'current-user' : ''}
+    >
       <div className="border-b-[1px] py-4">
         <Container>
           <div className="flex items-center justify-between gap-3 md:gap-0">
