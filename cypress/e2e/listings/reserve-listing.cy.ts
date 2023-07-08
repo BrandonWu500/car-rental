a19aa03c64d6ddd29eb5c6c01a20e2befc54aab6
BrandonWu500 <brwu17@gmail.com>
2023-07-08 14:46:25 -0400

    test: same-day reservation

--- cypress/e2e/listings/reserve-listing.cy.ts
+++ cypress/e2e/listings/reserve-listing.cy.ts
@@ -22,6 +22,31 @@ describe('Reserve listing user flow', () => {
 
     cy.findByRole('heading', { name: 'Login' }).should('exist');
   });
+
+  it('should create a same-day reservation if user clicks reserve button without selecting date range', () => {
+    cy.login();
+
+    // CLICK ON LISTING CARD
+    cy.findByText(/honda civic/i).click();
+
+    // FIND TITLE ON LISTING PAGE
+    cy.findByRole('heading', {
+      name: /honda civic - type r/i,
+    }).should('exist');
+
+    cy.findByRole('button', {
+      name: /^reserve$/i,
+    }).click();
+
+    cy.findByText(/listing reserved/i).should('exist');
+
+    // CONFIRM REDIRECT TO TRIPS PAGE
+    cy.findByRole('heading', { name: 'Trips' }).should('exist');
+
+    cy.findByRole('heading', {
+      name: /honda civic/i,
+    }).should('exist');
+  });
 });
 
 export {};
