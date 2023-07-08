7bc2249a1cbed123374a755c224b7161394902c1
BrandonWu500 <brwu17@gmail.com>
2023-07-08 11:55:04 -0400

    test: reserve listing when not logged in

--- cypress/e2e/listings/reserve-listing.cy.ts
+++ cypress/e2e/listings/reserve-listing.cy.ts
@@ -4,6 +4,24 @@ describe('Reserve listing user flow', () => {
   beforeEach(() => {
     cy.resetDB();
   });
+
+  it('should open login modal when "reserve" button is clicked and user is not logged in', () => {
+    cy.visit('/');
+
+    // CLICK ON LISTING CARD
+    cy.findByText(/honda civic/i).click();
+
+    // FIND TITLE ON LISTING PAGE
+    cy.findByRole('heading', {
+      name: /honda civic - type r/i,
+    }).should('exist');
+
+    cy.findByRole('button', {
+      name: /^reserve$/i,
+    }).click();
+
+    cy.findByRole('heading', { name: 'Login' }).should('exist');
+  });
 });
 
 export {};
