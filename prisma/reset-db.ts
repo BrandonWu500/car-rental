e026d81c93890617c3b6188a0dd0e85f3be614b0
BrandonWu500 <brwu17@gmail.com>
2023-07-08 10:00:29 -0400

    chore: create createTestListing function

--- prisma/reset-db.ts
+++ prisma/reset-db.ts
@@ -1,4 +1,5 @@
 import { hash } from 'bcrypt';
+
 import { prisma } from './../libs/prismadb';
 
 const deleteAll = async () => {
@@ -8,7 +9,7 @@ const deleteAll = async () => {
 };
 
 const createTestUser = async (name: string, password: string) => {
-  await prisma.user.create({
+  return await prisma.user.create({
     data: {
       email: `${name}@test.com`,
       name,
@@ -17,8 +18,31 @@ const createTestUser = async (name: string, password: string) => {
   });
 };
 
+const createTestListing = async (userId: string) => {
+  const listingInputData = {
+    category: 'Sports Cars',
+    imageSrc:
+      'https://res.cloudinary.com/dqrdsleqt/image/upload/v1687955203/zl55ycdkze7q2tvcxg9r.jpg',
+    info: "It's really fun to drive!",
+    state: 'CA',
+    city: 'Los Angeles',
+    make: 'Honda',
+    model: 'Civic',
+    passengerCount: 5,
+    price: 250,
+    trim: 'Type R',
+    userId,
+  };
+
+  await prisma.listing.create({
+    data: listingInputData,
+  });
+};
+
 export const resetDB = async () => {
   await deleteAll();
-  await createTestUser('john', '123456');
+  const user1 = await createTestUser('john', '123456');
   await createTestUser('jane', '123456');
+
+  await createTestListing(user1.id);
 };
