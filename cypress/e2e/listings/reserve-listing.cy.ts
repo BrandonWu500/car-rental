1c9738f5e85066b65306f15b4fec87e0ec832bb1
BrandonWu500 <brwu17@gmail.com>
2023-07-08 18:26:07 -0400

    test: create reservation with valid date range

--- cypress/e2e/listings/reserve-listing.cy.ts
+++ cypress/e2e/listings/reserve-listing.cy.ts
@@ -47,6 +47,42 @@ describe('Reserve listing user flow', () => {
       name: /honda civic/i,
     }).should('exist');
   });
+
+  it('should create a reservation if user selects a valid date range', () => {
+    cy.login();
+
+    // CLICK ON LISTING CARD
+    cy.findByText(/honda civic/i).click();
+
+    // FIND TITLE ON LISTING PAGE
+    cy.findByRole('heading', {
+      name: /honda civic - type r/i,
+    }).should('exist');
+
+    // SELECT DATE RANGE
+
+    // first press next month button to ensure dates aren't disabled
+    cy.findByRole('button', {
+      name: /next month/i,
+    }).click();
+
+    // choose start and end date
+    cy.findByText('23').click();
+    cy.findByText('27').click();
+
+    cy.findByRole('button', {
+      name: /^reserve$/i,
+    }).click();
+
+    cy.findByText(/listing reserved/i).should('exist');
+
+    // CONFIRM REDIRECT TO TRIPS PAGE
+    cy.findByRole('heading', { name: 'Trips' }).should('exist');
+
+    cy.findByRole('heading', {
+      name: /honda civic/i,
+    }).should('exist');
+  });
 });
 
 export {};
