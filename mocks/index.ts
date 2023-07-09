c8eb03087aa9e4e573ddf47dd5a7a2b84a4c836a
BrandonWu500 <brwu17@gmail.com>
2023-07-09 19:47:28 -0400

    chore: create mock data file

---
+++ mocks/index.ts
@@ -0,0 +1,28 @@
+export const LISTING_INPUT_DATA = [
+  {
+    category: 'Sports Cars',
+    imageSrc:
+      'https://res.cloudinary.com/dqrdsleqt/image/upload/v1687955203/zl55ycdkze7q2tvcxg9r.jpg',
+    info: "It's really fun to drive!",
+    state: 'CA',
+    city: 'Los Angeles',
+    make: 'Honda',
+    model: 'Civic',
+    passengerCount: 5,
+    price: 250,
+    trim: 'Type R',
+  },
+  {
+    category: 'Crossovers & SUVs',
+    imageSrc:
+      'https://res.cloudinary.com/dqrdsleqt/image/upload/v1688238259/yhanrur1xkna0rjlatnv.jpg',
+    info: 'Great for winter driving!',
+    state: 'MI',
+    city: 'Ann Arbor',
+    make: 'Subaru',
+    model: 'Outback',
+    passengerCount: 5,
+    price: 120,
+    trim: 'Touring',
+  },
+];
