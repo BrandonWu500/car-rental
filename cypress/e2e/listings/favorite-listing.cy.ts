cc6aa961bea09c77a677e5304f06b5e052ec6345
BrandonWu500 <brwu17@gmail.com>
2023-07-09 19:24:12 -0400

    test: favorite listing

--- cypress/e2e/listings/favorite-listing.cy.ts
+++ cypress/e2e/listings/favorite-listing.cy.ts
@@ -5,6 +5,18 @@ describe('Favorite listing user flows', () => {
     cy.resetDB();
     cy.seed();
   });
+
+  it('should favorite a listing when the favorite button is clicked', () => {
+    cy.login('joe@test.com', '123456');
+
+    cy.findByRole('button', {
+      name: /^favorite$/i,
+    }).click();
+
+    cy.findByRole('button', {
+      name: /unfavorite/i,
+    }).should('exist');
+  });
 });
 
 export {};
