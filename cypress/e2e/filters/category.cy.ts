43561376f34acee1c08a868d277644674b469148
BrandonWu500 <brwu17@gmail.com>
2023-07-11 09:30:44 -0400

    test: category filter

--- cypress/e2e/filters/category.cy.ts
+++ cypress/e2e/filters/category.cy.ts
@@ -5,6 +5,35 @@ describe('Category filter', () => {
     cy.resetDB();
     cy.seed();
   });
+
+  it('should correctly show listings based on the category filter selected', () => {
+    cy.visit('/');
+
+    // EXPECT BOTH LISTINGS WHEN NO FILTERS SELECTED
+    cy.findByRole('heading', { name: /civic/i }).should('exist');
+    cy.findByRole('heading', { name: /outback/i }).should('exist');
+
+    // SELECT ELECTRIC FILTER
+    cy.findByTestId('category-filter').within(() => {
+      cy.findByText(/electric/i).click();
+    });
+    cy.findByRole('heading', { name: /civic/i }).should('not.exist');
+    cy.findByRole('heading', { name: /outback/i }).should('not.exist');
+
+    // SELECT SPORTS CARS FILTER
+    cy.findByTestId('category-filter').within(() => {
+      cy.findByText(/sports cars/i).click();
+    });
+    cy.findByRole('heading', { name: /civic/i }).should('exist');
+    cy.findByRole('heading', { name: /outback/i }).should('not.exist');
+
+    // SELECT CROSSOVERS & SUVS FILTER
+    cy.findByTestId('category-filter').within(() => {
+      cy.findByText('Crossovers & SUVs').click();
+    });
+    cy.findByRole('heading', { name: /civic/i }).should('not.exist');
+    cy.findByRole('heading', { name: /outback/i }).should('exist');
+  });
 });
 
 export {};
