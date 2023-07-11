143ae08cac20e8cdf99e5807a9b3e6b4836647ec
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-07-11 09:34:27 -0400

    test: trips page (#104)

---
+++ cypress/e2e/reservations/trips.cy.ts
@@ -0,0 +1,52 @@
+/// <reference types="Cypress" />
+
+describe('Trips', () => {
+  beforeEach(() => {
+    cy.resetDB();
+    cy.seed();
+  });
+
+  // jane user has one reservation for civic
+
+  it("should show the user's trips on the trips page", () => {
+    cy.login('jane@test.com', '123456');
+
+    cy.findByRole('button', { name: /user menu/i }).click();
+
+    cy.findByText(/my trips/i).click();
+
+    cy.url().should('include', '/trips');
+
+    cy.findByRole('heading', { name: /civic/i }).should('exist');
+    cy.findByRole('button', { name: /cancel reservation/i }).should('exist');
+  });
+
+  it('should show unauthorized if not logged in', () => {
+    cy.visit('/trips');
+
+    cy.findByRole('heading', { name: /unauthorized/i }).should('exist');
+
+    cy.findByRole('heading', { name: /civic/i }).should('not.exist');
+    cy.findByRole('button', { name: /cancel reservation/i }).should(
+      'not.exist'
+    );
+  });
+
+  it("should cancel the user's trip if cancel reservation is clicked", () => {
+    cy.login('jane@test.com', '123456');
+
+    cy.findByRole('button', { name: /user menu/i }).click();
+
+    cy.findByText(/my trips/i).click();
+
+    cy.url().should('include', '/trips');
+
+    cy.findByRole('heading', { name: /civic/i }).should('exist');
+    cy.findByRole('button', { name: /cancel reservation/i }).click();
+
+    cy.findByRole('heading', { name: /civic/i }).should('not.exist');
+    cy.findByRole('heading', { name: /no trips found/i }).should('exist');
+  });
+});
+
+export {};
