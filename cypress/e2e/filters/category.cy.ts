dc2ae249db0c6151b9121cfc58d11ca13eccab6c
BrandonWu500 <brwu17@gmail.com>
2023-07-11 09:34:01 -0400

    test: remove category filter

--- cypress/e2e/filters/category.cy.ts
+++ cypress/e2e/filters/category.cy.ts
@@ -34,6 +34,42 @@ describe('Category filter', () => {
     cy.findByRole('heading', { name: /civic/i }).should('not.exist');
     cy.findByRole('heading', { name: /outback/i }).should('exist');
   });
+
+  it('should remove category filter when currently selected category is clicked again', () => {
+    cy.visit('/');
+
+    // EXPECT BOTH LISTINGS WHEN NO FILTERS SELECTED
+    cy.findByRole('heading', { name: /civic/i }).should('exist');
+    cy.findByRole('heading', { name: /outback/i }).should('exist');
+
+    // SELECT ELECTRIC FILTER
+    cy.findByTestId('category-filter').within(() => {
+      cy.findByText(/electric/i).click();
+    });
+    cy.findByRole('heading', { name: /civic/i }).should('not.exist');
+    cy.findByRole('heading', { name: /outback/i }).should('not.exist');
+
+    // DE-SELECT ELECTRIC FILTER
+    cy.findByTestId('category-filter').within(() => {
+      cy.findByText(/electric/i).click();
+    });
+    cy.findByRole('heading', { name: /civic/i }).should('exist');
+    cy.findByRole('heading', { name: /outback/i }).should('exist');
+
+    // SELECT SPORTS CARS FILTER
+    cy.findByTestId('category-filter').within(() => {
+      cy.findByText(/sports cars/i).click();
+    });
+    cy.findByRole('heading', { name: /civic/i }).should('exist');
+    cy.findByRole('heading', { name: /outback/i }).should('not.exist');
+
+    // DE-SELECT SPORTS CARS FILTER
+    cy.findByTestId('category-filter').within(() => {
+      cy.findByText(/sports cars/i).click();
+    });
+    cy.findByRole('heading', { name: /civic/i }).should('exist');
+    cy.findByRole('heading', { name: /outback/i }).should('exist');
+  });
 });
 
 export {};
