d0c7b2f2bef63e3674c4b9d31301a46dff5bce26
BrandonWu500 <brwu17@gmail.com>
2023-07-11 09:12:02 -0400

    test: show unauthorized if not logged in

--- cypress/e2e/reservations/trips.cy.ts
+++ cypress/e2e/reservations/trips.cy.ts
@@ -20,6 +20,17 @@ describe('Trips', () => {
     cy.findByRole('heading', { name: /civic/i }).should('exist');
     cy.findByRole('button', { name: /cancel reservation/i }).should('exist');
   });
+
+  it('should show unauthorized if not logged in', () => {
+    cy.visit('/trips');
+
+    cy.findByRole('heading', { name: /unauthorized/i }).should('exist');
+
+    cy.findByRole('heading', { name: /civic/i }).should('not.exist');
+    cy.findByRole('button', { name: /cancel reservation/i }).should(
+      'not.exist'
+    );
+  });
 });
 
 export {};
