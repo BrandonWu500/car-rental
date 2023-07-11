2a02c97e8f01f6f236ecfe4f6109981178ea37d6
BrandonWu500 <brwu17@gmail.com>
2023-07-11 07:19:26 -0400

    test: unfavorite listing

--- cypress/e2e/listings/favorite-listing.cy.ts
+++ cypress/e2e/listings/favorite-listing.cy.ts
@@ -6,16 +6,38 @@ describe('Favorite listing user flows', () => {
     cy.seed();
   });
 
+  // outback is already favorited, civic is not
+
   it('should favorite a listing when the favorite button is clicked', () => {
     cy.login('joe@test.com', '123456');
 
-    cy.findByRole('button', {
-      name: /^favorite$/i,
-    }).click();
+    cy.findByRole('heading', { name: /civic/i })
+      .parent()
+      .within(() => {
+        cy.findByRole('button', {
+          name: /^favorite$/i,
+        }).click();
 
-    cy.findByRole('button', {
-      name: /unfavorite/i,
-    }).should('exist');
+        cy.findByRole('button', {
+          name: /unfavorite/i,
+        }).should('exist');
+      });
+  });
+
+  it('should unfavorite a listing when the unfavorite button is clicked', () => {
+    cy.login('joe@test.com', '123456');
+
+    cy.findByRole('heading', { name: /outback/i })
+      .parent()
+      .within(() => {
+        cy.findByRole('button', {
+          name: /unfavorite/i,
+        }).click();
+
+        cy.findByRole('button', {
+          name: /^favorite$/i,
+        }).should('exist');
+      });
   });
 });
 
