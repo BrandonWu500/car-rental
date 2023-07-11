1d912ace416e740c02b2a97294fd01160e5c44d0
BrandonWu500 <brwu17@gmail.com>
2023-07-11 10:23:57 -0400

    test: correctly update search filters display

--- cypress/e2e/filters/search.cy.ts
+++ cypress/e2e/filters/search.cy.ts
@@ -5,6 +5,46 @@ describe('Search filters', () => {
     cy.resetDB();
     cy.seed();
   });
+
+  it('should correctly update search filters display based on the search filters selected', () => {
+    cy.visit('/');
+
+    cy.findByTestId('search-filters').within(() => {
+      cy.findByText('Anywhere in US').should('exist');
+      cy.findByText('Any Week').should('exist');
+      cy.findByText('Add Passengers').should('exist');
+    });
+
+    cy.findByRole('button', { name: /search/i }).click();
+    cy.findByRole('heading', { name: /filters/i }).should('exist');
+
+    // LOCATION FILTERS
+    cy.findByRole('combobox').type('MA');
+    cy.findByRole('combobox').type('{enter}');
+    cy.findByRole('button', { name: /next/i }).click();
+
+    cy.findByRole('combobox').type('Boston');
+    cy.findByRole('combobox').type('{enter}');
+    cy.findByRole('button', { name: /next/i }).click();
+
+    // DATE FILTER
+    // DEFAULT SELECT IS SAME DAY
+    cy.findByRole('button', { name: /next/i }).click();
+
+    // PASSENGER FILTER
+    for (let i = 0; i < 4; i++) {
+      cy.findByRole('button', { name: /add/i }).click();
+    }
+    cy.findByTestId('modal').within(() => {
+      cy.findByRole('button', { name: /search/i }).click();
+    });
+
+    cy.findByTestId('search-filters').within(() => {
+      cy.findByText('Boston, MA').should('exist');
+      cy.findByText('1 day').should('exist');
+      cy.findByText('5 passengers').should('exist');
+    });
+  });
 });
 
 export {};
