487ad21388f66c7b7e5b632f6a1a50b6cfa165ca
BrandonWu500 <brwu17@gmail.com>
2023-07-11 09:11:32 -0400

    test: correctly shows user trips

--- cypress/e2e/reservations/trips.cy.ts
+++ cypress/e2e/reservations/trips.cy.ts
@@ -5,6 +5,21 @@ describe('Trips', () => {
     cy.resetDB();
     cy.seed();
   });
+
+  // jane user has one reservation for civic
+
+  it("should show the user's trips on the trips page", () => {
+    cy.login('jane@test.com', '123456');
+
+    cy.findByRole('button', { name: /user menu/i }).click();
+
+    cy.findByText(/my trips/i).click();
+
+    cy.url().should('include', '/trips');
+
+    cy.findByRole('heading', { name: /civic/i }).should('exist');
+    cy.findByRole('button', { name: /cancel reservation/i }).should('exist');
+  });
 });
 
 export {};
