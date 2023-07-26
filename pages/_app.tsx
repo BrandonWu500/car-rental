20f61d8fe0af0e94dc1fc3ef64582015acec6b14
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-07-26 08:33:23 -0400

    feat: improve seo (#111)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,20 @@ import { Nunito } from 'next/font/google';
 
 import Layout from '@/components/layout/Layout';
 import '@/styles/global.css';
+import Head from 'next/head';
 
 const font = Nunito({ subsets: ['latin'] });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className={font.className}>
+      <Head>
+        <title>Car Rentals - Car Rental</title>
+        <meta
+          name="description"
+          content="Rent cars listed by other users or list your own cars for other users to rent"
+        />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
