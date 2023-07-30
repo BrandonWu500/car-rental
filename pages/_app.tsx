6296166e26e72f772b5991082c8e0e6650d6a49a
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-07-30 12:26:41 -0400

    feat: add google analytics (#123)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,9 @@ import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 import { Nunito } from 'next/font/google';
 import Head from 'next/head';
+import Script from 'next/script';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 import Layout from '@/components/layout/Layout';
 
@@ -10,6 +13,12 @@ import '@/styles/global.css';
 const font = Nunito({ subsets: ['latin'] });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: `${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}`,
+    });
+  }, []);
+
   return (
     <div className={font.className}>
       <Head>
@@ -22,7 +31,24 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+
+      {/* VERCEL ANALYTICS */}
       <Analytics />
+
+      {/* GOOGLE ANALYTICS */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+        `}
+      </Script>
     </div>
   );
 }
