169d7d05bd4684275c59d2e36c9d5d4a89438ad3
BrandonWu500 <brwu17@gmail.com>
2023-08-01 18:38:56 -0400

    feat: add paginate listings logic to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
+import Button from '@/components/Button';
 import Container from '@/components/Container';
 import EmptyState from '@/components/EmptyState';
 import ListingCard from '@/components/listings/ListingCard';
+import { LISTINGS_PAGE_SIZE } from '@/constants';
 
 import { prisma } from '@/libs/prismadb';
 import { SafeTypeListing } from '@/types';
+import { paginate } from '@/utils/paginate';
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import { useMemo, useState } from 'react';
 
 export const getServerSideProps: GetServerSideProps = async (
   ctx: GetServerSidePropsContext
@@ -70,6 +74,17 @@ interface HomeProps {
 }
 
 const Home = ({ listings }: HomeProps) => {
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const paginatedListings = useMemo(
+    () => paginate<SafeTypeListing>(listings, pageNumber, LISTINGS_PAGE_SIZE),
+    [listings, pageNumber]
+  );
+
+  const handleLoadMore = () => {
+    setPageNumber(pageNumber + 1);
+  };
+
   if (!listings.length) {
     return <EmptyState showReset />;
   }
@@ -77,14 +92,22 @@ const Home = ({ listings }: HomeProps) => {
   return (
     <Container>
       <div
-        className="grid grid-cols-1 gap-8 pt-24
-    sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5
-    2xl:grid-cols-6"
+        className="grid grid-cols-1 gap-16 pt-24
+    sm:grid-cols-2 md:grid-cols-3"
       >
-        {listings?.map((listing) => (
+        {paginatedListings?.map((listing) => (
           <ListingCard key={listing.id} listing={listing} />
         ))}
       </div>
+      {listings.length > paginatedListings.length && (
+        <div className="mx-auto mt-16 w-fit">
+          <Button
+            label="Load More"
+            onClick={handleLoadMore}
+            fullWidth={false}
+          />
+        </div>
+      )}
     </Container>
   );
 };
