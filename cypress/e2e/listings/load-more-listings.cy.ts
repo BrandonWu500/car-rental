159a87e9e14cea63b65145ec1a6a343094da43b7
BrandonWu500 <brwu17@gmail.com>
2023-08-02 11:45:23 -0400

    test: hide load more button when no more listings left

--- cypress/e2e/listings/load-more-listings.cy.ts
+++ cypress/e2e/listings/load-more-listings.cy.ts
@@ -10,6 +10,16 @@ describe('Load more listings user flow', () => {
     cy.findByRole('button', { name: /load more/i }).click();
     cy.findAllByRole('img', { name: /listing/i }).should('have.length', 6);
   });
+
+  it('should not show the load more button if there are no more listings left', () => {
+    cy.visit('/');
+    cy.findAllByRole('img', { name: /listing/i }).should('have.length', 3);
+    cy.findByRole('button', { name: /load more/i }).click();
+    cy.findAllByRole('img', { name: /listing/i }).should('have.length', 6);
+    cy.findByRole('button', { name: /load more/i }).click();
+    cy.findAllByRole('img', { name: /listing/i }).should('have.length', 8);
+    cy.findByRole('button', { name: /load more/i }).should('not.exist');
+  });
 });
 
 export {};
