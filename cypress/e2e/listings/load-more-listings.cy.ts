af6b5c380dddd68c49b91ac7cff7edc7bf9a30a7
BrandonWu500 <brwu17@gmail.com>
2023-08-02 11:43:00 -0400

    test: loads at most 3 more listings at a time

--- cypress/e2e/listings/load-more-listings.cy.ts
+++ cypress/e2e/listings/load-more-listings.cy.ts
@@ -3,6 +3,13 @@ describe('Load more listings user flow', () => {
     cy.resetDB();
     cy.seedMany(); // seeds with 8 listings
   });
+
+  it('should load at most 3 more listings when the user presses the load more button', () => {
+    cy.visit('/');
+    cy.findAllByRole('img', { name: /listing/i }).should('have.length', 3);
+    cy.findByRole('button', { name: /load more/i }).click();
+    cy.findAllByRole('img', { name: /listing/i }).should('have.length', 6);
+  });
 });
 
 export {};
