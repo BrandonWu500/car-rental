a58f619b1dfe8678864bd3ea7b431074107ad776
BrandonWu500 <brwu17@gmail.com>
2023-08-02 11:23:54 -0400

    chore: create seedMany function
    which will create more listings to test pagination

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -67,3 +67,22 @@ export const seed = async () => {
     console.error(error);
   }
 };
+
+export const seedMany = async () => {
+  try {
+    const user1 = await createTestUser('joe', '123456');
+    const user2 = await createTestUser('jane', '123456');
+
+    await createTestListing(user1.id, 0);
+    await createTestListing(user2.id, 1);
+    await createTestListing(user1.id, 0);
+    await createTestListing(user2.id, 1);
+    await createTestListing(user1.id, 0);
+    await createTestListing(user2.id, 1);
+    await createTestListing(user1.id, 0);
+    await createTestListing(user2.id, 1);
+  } catch (error) {
+    console.error('Failed to seed DB');
+    console.error(error);
+  }
+};
